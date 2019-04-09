2850be158a1cd982a88ee7232727b189ffd7a78d
Tasar <47983294+tasarweb@users.noreply.github.com>
2019-04-09 19:18:26 +0430

    add getThumbnailAttribute
    add getThumbnailAttribute

--- src/Models/TasarTheme.php
+++ src/Models/TasarTheme.php
@@ -8,4 +8,10 @@ class TasarTheme extends Model
 {
     public $table = 'tasar_themes';
     protected $guarded = ['id'];
+    
+    public function getThumbnailAttribute()
+    {
+        $thumbnail = Theme::getThemeInfo($this->dir, 'thumbnail');
+        return $thumbnail ? theme_url($thumbnail) : url('assets/images/default_theme_thumbnail.jpg');
+    }
 }
